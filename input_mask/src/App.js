c74863f45cfcfe5ebe558e76884873a53df17280
alireza <alireza.jhd2000@gmail.com>
2021-09-21 17:51:33 +0430

    Solve input_mask question

--- input_mask/src/App.js
+++ input_mask/src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import Input from "./Input";
+import cities from "./cities.json";
 
 function App() {
-   return <div></div>
+  const [hint, setHint] = useState("");
+
+  const handleChange = (e) => {
+    const typedCity = e.target.value;
+
+    if (!typedCity) {
+      setHint("");
+    } else {
+      const matched = cities.find((city) => city.startsWith(typedCity));
+
+      if (matched) {
+        setHint(matched);
+      } else {
+        setHint("");
+      }
+    }
+  };
+  return (
+    <div>
+      <Input handleChange={handleChange} hint={hint} />
+    </div>
+  );
 }
 
 export default App;
