345abafb55ded0e6a301e35347437470df22a53c
alireza <alireza.jhd2000@gmail.com>
2021-09-21 17:13:07 +0430

    Solve quera_chat question

--- quera_chat/src/components/MessageForm/MessageForm.js
+++ quera_chat/src/components/MessageForm/MessageForm.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MessageForm.css";
 
 function MessageForm({ onMessageSend }) {
-  function handleFormSubmit(event) {}
+  const [message, setMessage] = useState("");
+
+  function handleFormSubmit(event) {
+    const trimmedMessage = message.trim();
+    if (trimmedMessage) {
+      onMessageSend(trimmedMessage);
+      setMessage("");
+    }
+  }
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
   return (
     <form
       className="MessageForm"
@@ -11,9 +23,12 @@ function MessageForm({ onMessageSend }) {
     >
       <div className="input-container">
         <input
+          value={message}
+          autoFocus
           data-testid="input-message"
           type="text"
           placeholder="پیام خود را اینجا بنویسید..."
+          onChange={handleChange}
         />
       </div>
       <div className="button-container">
--- quera_chat/src/containers/App.js
+++ quera_chat/src/containers/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MessageForm from "../components/MessageForm/MessageForm";
 import MessageList from "../components/MessageList/MessageList";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    let cleanupStarted = false;
+    if (messages.length > 0 && messages[messages.length - 1].me === true) {
+      const lastMessage = messages[messages.length - 1].body;
+      fetch(`http://localhost:3001/message/${lastMessage}`)
+        .then((response) => response.json())
+        .then((response) => response.message)
+        .then((answeredMessage) => {
+          if (!cleanupStarted) {
+            setMessages([...messages, { me: false, body: answeredMessage }]);
+          }
+        });
+    }
+    return () => (cleanupStarted = true);
+  }, [messages]);
+
+  const onMessageSend = (message) => {
+    setMessages((prevMessages) => [...messages, { me: true, body: message }]);
+  };
+
+  return (
+    <div className="App">
+      <MessageList messages={messages} />
+      <MessageForm onMessageSend={onMessageSend} />
+    </div>
+  );
 }
 
 export default App;
