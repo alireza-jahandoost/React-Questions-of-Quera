c3d4fcfec01aba59398afcb52d935f69deddcf48
alireza <alireza.jhd2000@gmail.com>
2021-09-21 17:29:17 +0430

    Solve theme-switcher question

--- theme-switcher/src/components/ThemeSwitcher.jsx
+++ theme-switcher/src/components/ThemeSwitcher.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { THEME_TYPE } from "../constants";
+import { ThemeContext } from "../providers/ThemeProvider";
 
 const ThemeSwitcher = () => {
   // No need to change *return* part
   // You have to set themeMode based on context
-  const themeMode = THEME_TYPE.LIGHT;
-  
-  const handleThemeChange = (e) => {};
+  const { themeMode, changeThemeMode } = useContext(ThemeContext);
+
+  const handleThemeChange = (e) => {
+    const darkMode = e.target.checked;
+    if (darkMode) {
+      changeThemeMode(THEME_TYPE.DARK);
+    } else {
+      changeThemeMode(THEME_TYPE.LIGHT);
+    }
+  };
 
   return (
     <div className="switch-container">
--- theme-switcher/src/providers/ThemeProvider.jsx
+++ theme-switcher/src/providers/ThemeProvider.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { createContext, useState } from "react";
+import { THEME_TYPE } from "../constants";
+
+export const ThemeContext = createContext();
+
 const ThemeProvider = ({ children }) => {
-  return <>{children}</>;
+  const [themeMode, setThemeMode] = useState(THEME_TYPE.LIGHT);
+
+  const changeThemeMode = (newTheme) => {
+    setThemeMode(newTheme);
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        themeMode,
+        changeThemeMode,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 export default ThemeProvider;
