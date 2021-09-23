b71fced9091187177d553dab3598e00391b2b65c
alireza <alireza.jhd2000@gmail.com>
2021-09-23 20:05:49 +0330

    Solve password-input question

--- password-input/src/Containers/Password.js
+++ password-input/src/Containers/Password.js
@@ -1,14 +1,39 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
+import Bar from "../Components/Bar";
+import Input from "../Components/Input";
+import zxcvbn from "zxcvbn";
 
+const Password = ({ strength }) => {
+  const [password, setPassword] = useState("");
+  const [passwordScore, setPasswordScore] = useState(-1);
+  const [suggestion, setSuggestion] = useState("");
 
-class Password extends Component {
-    render() {
-        return (
-            <p>Password Input</p>
-        )
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+    if (value === "") {
+      setPasswordScore(-1);
+      setSuggestion("");
+    } else {
+      const { score, feedback } = zxcvbn(value);
+      setPasswordScore(score);
+      setSuggestion(feedback.suggestions.join(""));
     }
+  };
 
+  const { text, bgColor } =
+    passwordScore === -1
+      ? { text: "", bgColor: "white" }
+      : strength[passwordScore];
 
-}
+  return (
+    <div>
+      <Input type="text" value={password} onChange={handleChange} />
+      <Bar width={(passwordScore + 1) * 140} height={"5px"} bgColor={bgColor} />
+      <h4 id="result">{passwordScore === -1 ? "Enter Your Password" : text}</h4>
+      <p id="suggestion">{suggestion}</p>
+    </div>
+  );
+};
 
 export default Password;
