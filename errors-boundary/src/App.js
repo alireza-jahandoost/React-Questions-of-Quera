2f37daf0337b63a2c7772ebf26bed4c2b5418413
alireza <alireza.jhd2000@gmail.com>
2021-10-06 10:23:00 +0330

    Solve errors-boundary question

--- errors-boundary/src/App.js
+++ errors-boundary/src/App.js
@@ -1,25 +1,45 @@
-import React,{PureComponent} from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Form from "./Form";
 import ErrorBoundary from "./ErrorBoundary";
-class App extends PureComponent {
-
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       person: {
-        year : ''
+        year: "",
       },
-       error:''
-    }
+      error: "",
+    };
   }
+
+  handleInputChange = (e) => {
+    this.setState({ person: { year: e.target.value } });
+  };
+
+  handleReloadComponent = () => {
+    this.setState({ person: { year: "" }, error: "" });
+  };
+
+  changeError = (err) => {
+    this.setState({ error: err });
+  };
   render() {
     return (
-        <div className="App">
-          <header className="App-header">
-             <Form person={this.state.person} />
-          </header>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <ErrorBoundary
+            changeError={this.changeError}
+            error={this.state.error}
+            onReloadButtonPressed={this.handleReloadComponent}
+          >
+            <Form
+              person={this.state.person}
+              handleChange={this.handleInputChange}
+            />
+          </ErrorBoundary>
+        </header>
+      </div>
     );
   }
 }
--- errors-boundary/src/ErrorBoundary.js
+++ errors-boundary/src/ErrorBoundary.js
@@ -1,5 +1,13 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 import Error from "./Error";
 export default class ErrorBoundary extends Component {
-     render (){return(<div></div>)}
+  componentDidCatch(error) {
+    this.props.changeError(error);
+  }
+  render() {
+    if (this.props.error) {
+      return <Error reloadBtnOnClick={this.props.onReloadButtonPressed} />;
+    }
+    return <>{this.props.children}</>;
+  }
 }
