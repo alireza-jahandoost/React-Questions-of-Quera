387c94a832b9c4792c150bc8399934c6d8ad06d7
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2022-02-04 16:01:40 +0330

    Solve poll-creator question

--- poll-creator/src/components/DescriptionItem.js
+++ poll-creator/src/components/DescriptionItem.js
@@ -1,5 +1,65 @@
 import { ShowEmoji } from "./ShowEmoji";
+import { useEffect, useState } from "react";
 
 export const DescriptionItem = ({ emojis, setEmojis }) => {
-  return null
+  const [prevEmojis, setPrevEmojis] = useState(emojis);
+  useEffect(() => {
+    for (const e of emojis) {
+      console.log(
+        e,
+        prevEmojis.find((prev) => prev.title === e.title)
+      );
+      if (
+        e.isSelected === false &&
+        prevEmojis.find((prev) => prev.title === e.title).isSelected === true
+      ) {
+        if (e.description !== "") {
+          setEmojis([
+            ...emojis.map((currentEmoji) =>
+              currentEmoji.title === e.title
+                ? { ...currentEmoji, description: "" }
+                : currentEmoji
+            ),
+          ]);
+        }
+      }
+    }
+    let hasChange = false;
+    for (const e of emojis) {
+      const x = prevEmojis.find((prev) => prev.title === e.title);
+      if (x.isSelected !== e.isSelected || x.description !== e.description) {
+        hasChange = true;
+      }
+    }
+    if (hasChange) {
+      setPrevEmojis(emojis);
+    }
+  }, [emojis, prevEmojis, setEmojis]);
+  return (
+    <div>
+      {emojis.map((e) =>
+        !e.isSelected ? null : (
+          <div key={e.title}>
+            <label htmlFor={e.title}>
+              Description for
+              <ShowEmoji src={e.src} name={e.title} viewStyle="description" />:
+            </label>
+            <input
+              id={e.title}
+              value={e.description}
+              onChange={(event) =>
+                setEmojis([
+                  ...emojis.map((currentEmoji) =>
+                    currentEmoji.title === e.title
+                      ? { ...currentEmoji, description: event.target.value }
+                      : currentEmoji
+                  ),
+                ])
+              }
+            />
+          </div>
+        )
+      )}
+    </div>
+  );
 };
--- poll-creator/src/components/ShowPreviewEmoji.js
+++ poll-creator/src/components/ShowPreviewEmoji.js
@@ -1,5 +1,16 @@
 import { ShowEmoji } from "./ShowEmoji";
 
 export const ShowPreviewEmoji = ({ emojis }) => {
-  return null
+  return (
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
+      {emojis.map((e) =>
+        e.isSelected ? (
+          <div key={e.title}>
+            <ShowEmoji src={e.src} viewStyle="preview" name={e.title} />
+            <div style={{ textAlign: "center" }}>{e.description}</div>
+          </div>
+        ) : null
+      )}
+    </div>
+  );
 };
