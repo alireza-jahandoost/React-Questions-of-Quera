bcedd557604befc1ff1867a2e101bb754616f12e
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2022-02-05 10:54:08 +0330

    Solve simple-timer question

--- simple-timer/src/Timer.js
+++ simple-timer/src/Timer.js
@@ -1,12 +1,56 @@
-import React, {Component} from 'react';
-import './Timer.css';
+import React, { Component } from "react";
+import "./Timer.css";
 
 export class Timer extends Component {
-    render() {
-        return <div className="container">
-            <div className="timer">
-            </div>
-            <button>Reset Timer</button>
-        </div>;
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      currentInterval: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      currentInterval: setInterval(
+        () =>
+          this.setState((prevState) => ({
+            time: prevState.time + 1,
+          })),
+        1000
+      ),
+    });
+  }
+  componentWillUnmount() {
+    if (this.state.currentInterval !== null) {
+      clearInterval(this.state.currentInterval);
     }
+  }
+
+  clearTime() {
+    if (this.state.currentInterval !== null) {
+      clearInterval(this.state.currentInterval);
+    }
+    this.setState({
+      currentInterval: setInterval(
+        () =>
+          this.setState((prevState) => ({
+            time: prevState.time + 1,
+          })),
+        1000
+      ),
+      time: 0,
+    });
+  }
+
+  render() {
+    const { time } = this.state;
+
+    return (
+      <div className="container">
+        <div className="timer">{time}</div>
+        <button onClick={() => this.clearTime()}>Reset Timer</button>
+      </div>
+    );
+  }
 }
