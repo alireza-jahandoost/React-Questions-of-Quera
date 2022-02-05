51c9b7cf29c85a39ec052c9c73ccd23e253220b5
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2022-02-05 12:55:01 +0330

    Solve simple-hooks question

--- simple-hooks/src/hooks.js
+++ simple-hooks/src/hooks.js
@@ -1,10 +1,31 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 export function useStack() {
-    // ...
+  const [stack, setStack] = useState([]);
+
+  const push = (newElement) => {
+    setStack((prevElements) => [...prevElements, newElement]);
+  };
+
+  const pop = () => {
+    const lastElement = stack[stack.length - 1];
+    setStack((prevElements) => [
+      ...prevElements.slice(0, prevElements.length - 1),
+    ]);
+    return lastElement;
+  };
+
+  return { stack, push, pop };
 }
 
 export function useCounter(start, finish) {
-    // ...
+  const [currentTime, setCurrentTime] = useState(start);
+  const count = () => {
+    if (currentTime === finish) {
+      setCurrentTime(start);
+    } else {
+      setCurrentTime((prevTime) => prevTime + 1);
+    }
+  };
+  return [currentTime, count];
 }
-
