1cab86c14588c980e74db38ade0240508f68a92d
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2022-02-05 11:28:25 +0330

    Solve card-memory-game question

--- card-memory-game/src/components/Game.js
+++ card-memory-game/src/components/Game.js
@@ -1,9 +1,54 @@
+import { useState, useEffect } from "react";
 import Card from "./Card";
+import { cardsData } from "../cards";
 
 function Game() {
+  const [flipped, setFlipped] = useState([]);
+  const [isFreezed, setIsFreezed] = useState(0);
+  const [prevCard, setPrevCard] = useState(0);
+
+  const cardsAreSame = (firstId, secondId) => {
+    const firstName = cardsData.find((card) => card.id === firstId).name;
+    const secondName = cardsData.find((card) => card.id === secondId).name;
+    return firstName === secondName;
+  };
+
   return (
     <section className="memory-game">
-      {/* <Card card={card} onClick={} /> */}
+      {cardsData.map((card) => {
+        const { id, name, image } = card;
+        return (
+          <Card
+            key={id}
+            card={{ id, name, image, isFlipped: flipped.includes(id) }}
+            onClick={() => {
+              if (isFreezed || flipped.includes(id)) {
+                return;
+              }
+              if (prevCard === 0) {
+                setPrevCard(id);
+                setFlipped((prevState) => [...prevState, id]);
+              } else {
+                setFlipped((prevState) => [...prevState, id]);
+                if (cardsAreSame(prevCard, id)) {
+                  setPrevCard(0);
+                } else {
+                  setIsFreezed(1);
+                  setTimeout(() => {
+                    setPrevCard(0);
+                    setFlipped((prevState) => [
+                      ...prevState.filter(
+                        (current) => current !== id && current !== prevCard
+                      ),
+                    ]);
+                    setIsFreezed(0);
+                  }, 1500);
+                }
+              }
+            }}
+          />
+        );
+      })}
     </section>
   );
 }
